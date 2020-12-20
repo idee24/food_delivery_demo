f44d4feb43111f0b258609be2162dfe7fbe51230
idee1140 <idee1140@gmail.com>
2020-12-20 05:33:21 +0100

    filter_views

--- app/src/main/java/com/example/food_delivery_demo/fragments/MenuListFragment.kt
+++ app/src/main/java/com/example/food_delivery_demo/fragments/MenuListFragment.kt
@@ -1,10 +1,16 @@
 package com.example.food_delivery_demo.fragments
 
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.food_delivery_demo.MenuItem
@@ -20,7 +26,25 @@ class MenuListFragment : Fragment(R.layout.fragment_food_list) {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         initRecyclerView()
+
+        deployFilter("Vegan")
+        deployFilter("Low-Carb Diet")
     }
+
+    fun deployFilter(filterName: String) {
+
+        val layoutInflater = activity?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val itemView = layoutInflater.inflate(R.layout.filter_view, null)
+        val filterTextView: TextView = itemView.findViewById(R.id.filterTextView)
+        filterTextView.text = filterName
+
+        itemView.setOnClickListener {
+            itemView.setBackgroundResource(R.drawable.filter_selected)
+            filterTextView.setTextColor(Color.WHITE)
+        }
+        filterLayout.addView(itemView)
+    }
+
 
     fun initRecyclerView() {
 
